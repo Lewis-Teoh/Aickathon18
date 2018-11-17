091cd3aa630660ffdba459742b0f841bb22b81fb
Hao Siong <31809739+nghaosiong98@users.noreply.github.com>
2018-11-17 08:44:13 +0800

    Add many fragments

--- app/src/main/java/com/aichathon/aickathon2018/MainActivity.java
+++ app/src/main/java/com/aichathon/aickathon2018/MainActivity.java
@@ -1,13 +1,29 @@
 package com.aichathon.aickathon2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/aichathon/aickathon2018/SearchFragment.java
@@ -0,0 +1,45 @@
+package com.aichathon.aickathon2018;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchFragment extends android.app.Fragment {
+
+    private ImageButton cameraVector;
+    private ImageView uploadVector;
+
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        // Inflate the layout for this fragment
+        cameraVector = (ImageButton) view.findViewById(R.id.cameraVector);
+        cameraVector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("CLICK", "Clicked!");
+            }
+        });
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+
+}
